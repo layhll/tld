95a1de67301d456bc4f1470f22c5382819dcf2cb
liuchengguo <liuchengguo@we-te.com>
2017-02-27 10:37:27 +0800

    no message

---
+++ app/Jobs/Send.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tld\Wechat\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+
+class Send implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info(time());
+        //
+    }
+}
